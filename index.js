d61b741a6db03e3791feddeb6cf73a668264e439
oxica <kskaplun@gmail.com>
2022-02-08 10:25:37 +0200

    add

---
+++ index.js
@@ -0,0 +1,85 @@
+const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.getElementById("search-box");
+const listEl = document.querySelector(".country-list");
+const infoEl = document.querySelector(".country-info");
+
+const BASE_URL = "https://restcountries.com/v3.1/name";
+
+const fetchCountries = (name) => {
+  return fetch(
+    `${BASE_URL}/${name}?fields=name,capital,population,flags,languages`
+  ).then((response) => {
+    if (response.status === 404) {
+      return Promise.reject(new Error());
+    }
+    return response.json();
+  });
+};
+
+const cleanMarkup = (ref) => (ref.innerHTML = "");
+
+const inputHandler = (e) => {
+  const textInput = e.target.value.trim();
+
+  if (!textInput) {
+    cleanMarkup(listEl);
+    cleanMarkup(infoEl);
+    return;
+  }
+
+  fetchCountries(textInput)
+    .then((data) => {
+      console.log(data);
+      if (data.length > 10) {
+        console.log(
+          "Too many matches found. Please enter a more specific name"
+        );
+        return;
+      }
+      renderMarkup(data);
+    })
+    .catch((err) => {
+      cleanMarkup(listEl);
+      cleanMarkup(infoEl);
+      console.log("Oops, there is no country with that name");
+    });
+};
+
+const renderMarkup = (data) => {
+  if (data.length === 1) {
+    cleanMarkup(listEl);
+    const markupInfo = createInfoMarkup(data);
+    infoEl.innerHTML = markupInfo;
+  } else {
+    cleanMarkup(infoEl);
+    const markupList = createListMarkup(data);
+    listEl.innerHTML = markupList;
+  }
+};
+
+const createListMarkup = (data) => {
+  return data
+    .map(
+      ({ name, flags }) =>
+        `<li><img src="${flags.png}" alt="${name.official}" width="60" height="40">${name.official}</li>`
+    )
+    .join("");
+};
+
+const createInfoMarkup = (data) => {
+  return data.map(
+    ({ name, capital, population, flags, languages }) =>
+      `<h1><img src="${flags.png}" alt="${
+        name.official
+      }" width="40" height="40">${name.official}</h1>
+      <p>Capital: ${capital}</p>
+      <p>Population: ${population}</p>
+      <p>Languages: ${Object.values(languages)}</p>`
+  );
+};
+
+inputEl.addEventListener("input", inputHandler);
+
+
+
